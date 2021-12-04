49044825303501a869e6898dd395a20bfd5158cc
nicolamassarenti <nicola.massarenti@gmail.com>
2021-12-04 16:40:11 +0100

    Removed imports in __init__

--- package/wiser/gcloud/storage/__init__.py
+++ package/wiser/gcloud/storage/__init__.py
@@ -6,9 +6,3 @@ except ImportError:
     import pkgutil
 
     __path__ = pkgutil.extend_path(__path__, __name__)
-
-from wiser.gcloud.storage.services import Storage
-from wiser.gcloud.storage.connectors import StorageConnector
-from wiser.gcloud.storage.types import StorageLocationBuilder, StorageLocation
-
-__all__ = ["Storage", "StorageLocationBuilder", "StorageLocation", "StorageConnector"]
