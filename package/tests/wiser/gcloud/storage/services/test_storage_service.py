2c74e7b0b8a281f42774ce433398974b2d0c17f9
nicolamassarenti <nicola.massarenti@gmail.com>
2021-12-07 23:33:28 +0100

    added test get numpy

--- package/tests/wiser/gcloud/storage/services/test_storage_service.py
+++ package/tests/wiser/gcloud/storage/services/test_storage_service.py
@@ -66,3 +66,33 @@ class StorageServiceTest(unittest.TestCase):
         )
 
         self.assertEqual(Storage.get(location=location), data)
+
+    @patch(
+        "wiser.gcloud.storage.connectors.storage_connector.StorageConnector.download_to_filename"
+    )
+    def test_get_numpy(self, storage_mock):
+        """
+        GIVEN   a valid location pointing to a numpy array data
+        WHEN    Storage.get() is invoked
+        THEN    the expected data is returned
+        """
+        import numpy as np
+        from wiser.gcloud.storage.services import Storage
+        from wiser.gcloud.storage.types.location import StorageLocationBuilder
+
+        location = (
+            StorageLocationBuilder()
+            .set_bucket(bucket=BUCKET)
+            .set_blob_name(blob_name="path/to/data.npy")
+            .build()
+        )
+
+        data = np.array([1, 2, 3])
+
+        def write_to_file(filename, bucket_name, source_blob_name):
+            with open(filename, "wb") as f:
+                np.save(f, data)
+
+        storage_mock.side_effect = write_to_file
+
+        self.assertEqual(data.tolist(), Storage.get(location=location).tolist())
