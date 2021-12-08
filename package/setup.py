afc80be6409d0eccc3e4cb8de812d7095c0dd6f8
nicolamassarenti <nicola.massarenti@gmail.com>
2021-12-08 22:34:28 +0100

    Added version and licence

--- package/setup.py
+++ package/setup.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 CURRENT_DIR = Path(__file__).parent
 
+with open(CURRENT_DIR.joinpath("version.txt")) as f:
+    version = f.readlines()[0]
+
 # Package metadata
 name = "wiser-gcloud-storage"
-version = "0.0.6"
 author = "Nicola Massarenti"
 author_email = "nicola.massarenti@gmail.com"
 description = "Google Cloud Storage APIs for wiser"
